938b35898bfa41be2b6df953120625911cd7aae4
Vibhas Goyal <vibhas.goyal@oracle.com>
2023-04-01 20:18:58 +0530

    using lazygit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
+    let mut i = 10;
+    loop {
+        i = i + 1;
+        if i % 15 == 0 {
+            println!("fizz")
+        } else if i % 3 == 0 {
+            println!("buzz")
+        } else if i % 5 == 0 {
+            println!("fizzbuzz")
+        } else {
+            println!("{}", i);
+        }
+        if i > 100 {
+            break;
+        }
+    }
     println!("Hello, world!");
 }
